b128ca530cc657dc469b2d0a287aebf5b12777f8
zioscuro <lore.rang@gmail.com>
2022-08-15 21:44:37 +0200

    Add 04_removeFromArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, toRemove) {
+    const args = Array.prototype.slice.call(arguments);
+    const arrRemoveItems = args.slice(1);
+    
+    let filteredArr = [...arr];
+    for (let i = 0; i < arrRemoveItems.length; i++) {
+      
+      filteredArr = filteredArr.filter(item => item !== arrRemoveItems[i]);
+    }  
+    return filteredArr;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
