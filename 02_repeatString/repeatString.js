1b6146c1797f2504db294005ba3fadb22c6950e7
zioscuro <lore.rang@gmail.com>
2022-08-15 13:40:50 +0200

    Add 02_repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if (n<0) {
+        return 'ERROR'
+    }
+
+    let result = '';
+    for (let i=0; i < n; i++) {
+        result = result.concat(str);
+    }
+    return result;
 
 };
 
