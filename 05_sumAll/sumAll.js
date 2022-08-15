622433b7db924853cfff8870382bc196bd107e49
zioscuro <lore.rang@gmail.com>
2022-08-16 01:48:04 +0200

    Add 05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
 
+    const max = num1>num2?num1:num2;
+    const min = num1<num2?num1:num2;
+    let sum = 0;
+    
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
