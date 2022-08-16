3d0ab7510acc51c451acdfad1f658c102b293645
zioscuro <lore.rang@gmail.com>
2022-08-16 02:25:45 +0200

    Add 07_tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const celsiusTemp = ((temp -32) * 5) / 9;
+  const roundCelsiusTemp = Math.round(celsiusTemp * 10) / 10;
+  return roundCelsiusTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  //[°F] = [°C] × 9⁄5 + 32
+  const fahrenheitTemp = ((temp * 9) / 5) + 32;
+  const roundFahrenheitTemp = Math.round(fahrenheitTemp * 10) / 10;
+  return roundFahrenheitTemp;
 };
 
 // Do not edit below this line
