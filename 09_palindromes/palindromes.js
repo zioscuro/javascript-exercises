6cc5075cdf6054fad5f0a4defc2020e538f7d528
zioscuro <lore.rang@gmail.com>
2022-09-11 22:57:52 +0200

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  const regex = /[!,.\s]+/gi;
+  const testStr = str.replace(regex, '').toLowerCase()
+  for (let i = 0; i < testStr.length; i++) {
+    if (testStr[i] != testStr[(testStr.length - 1) - i]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
