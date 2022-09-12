01f3393ae6eddbdd8fdfc0cb344b35946c93c530
zioscuro <lore.rang@gmail.com>
2022-09-13 00:11:45 +0200

    Add solution for 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (library) {
+  const titles = [];
+  library.map((book) => {
+    titles.push(book.title);
+  });
+  return titles;
 };
 
 // Do not edit below this line
