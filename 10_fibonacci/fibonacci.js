98135f585d7091372b7f397f33041863667b3667
zioscuro <lore.rang@gmail.com>
2022-09-12 23:55:39 +0200

    Add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  if (num < 0) return 'OOPS';
 
+  let prev = 0;
+  let current = 1;
+  let counter = current + prev;
+  
+  for (let i = 1; i < num; i++) {
+    prev = current;
+    current = counter;
+    counter = current + prev;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
