d27287f8e3c1f970021309571fa85f8dfc7dc6eb
zioscuro <lore.rang@gmail.com>
2022-09-13 00:53:04 +0200

    Add solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const ages = arr.map((person) => {
+    if (!person.yearOfDeath) {
+      const now = new Date().getFullYear();
+      return now - person.yearOfBirth;
+    } else {
+      return person.yearOfDeath - person.yearOfBirth;
+    }
+  });
+  const indexMax = ages.indexOf(Math.max(...ages));
+  return arr[indexMax];
 };
 
 // Do not edit below this line
